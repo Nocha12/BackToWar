58c7f4e61385c37c2b76b3e1eb7ada7f4b8b1390
Nocha <jsyang789@gmail.com>
2017-06-11 00:53:57 +0900

    First Commit

---
+++ Assets/Script/FollowingCamera.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour {
+	public float distanceAway = 21;
+
+	private int touchId = -1;
+	public float speed = 0.1f;
+	public Vector2 nowPos, prePos;
+	public Vector3 movePos;
+
+	public PlayerMovement player;
+	private Transform follow;
+
+	void Start()
+	{
+		follow = player.transform;	
+	}
+
+	void Update()
+	{
+		int i = 0;
+
+		if (Input.touchCount > 0) {
+			foreach (Touch touch in Input.touches) {
+				i++;
+
+				if (touch.phase.Equals (TouchPhase.Began)) {
+					if (i != TouchPad.touchId) {
+						touchId = i;
+						prePos = touch.position - touch.deltaPosition;
+					}
+				}
+				if (touch.phase.Equals (TouchPhase.Moved) || touch.phase.Equals (TouchPhase.Stationary)) {
+					if (touchId.Equals (i)) {
+						nowPos = touch.position - touch.deltaPosition;
+						movePos = (Vector3)(prePos - nowPos) * speed;
+						transform.Translate(movePos);
+						prePos = touch.position - touch.deltaPosition;
+					}
+				}
+				if(touch.phase.Equals(TouchPhase.Ended))
+				{
+					if (touchId.Equals (i))
+						touchId = -1;
+				}
+			}
+		}
+	}
+
+	void LateUpdate () {
+		Vector3 relativePos = follow.position - transform.position;
+		transform.rotation = Quaternion.LookRotation (relativePos);
+
+		Vector3 diffVector = transform.position - follow.position;
+		diffVector.Normalize ();
+		transform.position = follow.position + diffVector * distanceAway;
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (new Vector3(0,1,0));
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (new Vector3(0,-1,0));
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (new Vector3(1,0,0));
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (new Vector3(-1,0,0));
+		}
+	}
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	protected Animator avatar;
+
+	float lastAttackTime, lastSkillTime, lastDashTime;
+
+	public bool attacking = false; 
+	public bool dashing = false; 
+
+	public Vector3 rotationOffset = new Vector3(-110, 0, -1.52f);
+
+	void Start () {
+		avatar = GetComponent<Animator>();
+	}
+
+	float h, v;
+
+	public void OnStickChanged(Vector2 stickPos)
+	{
+		h = stickPos.x;
+		v = stickPos.y;
+	}
+
+	void Update () {
+		if (!avatar)
+			return;
+	
+		avatar.SetFloat ("Speed", (h * h + v * v));
+
+		Rigidbody rigidbody = GetComponent<Rigidbody> ();
+
+		if (rigidbody) {
+			Vector3 speed = rigidbody.velocity;
+			speed.x = 4 * h;
+			speed.z = 4 * v;
+			rigidbody.velocity = speed;
+
+			if (h != 0 && v != 0) {
+				transform.rotation = Quaternion.LookRotation (new Vector3 (h, 0, v));
+				transform.eulerAngles = transform.eulerAngles + rotationOffset;
+			}
+		}
+	}
+}
